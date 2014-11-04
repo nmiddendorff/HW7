d3580b8df2f0821b1370057d0adc6df7a2649c42
nmiddendorff <middendorff.nelson@gmail.com>
2014-11-03 22:02:47 -0600

    Role Based Security configured
    vb code references viewAllCars.aspx

---
+++ login/login.aspx.vb
@@ -0,0 +1,11 @@
+﻿
+Partial Class login_login
+    Inherits System.Web.UI.Page
+
+    Protected Sub Login1_LoggedIn(sender As Object, e As EventArgs) Handles Login1.LoggedIn
+        If Roles.IsUserInRole(Login1.UserName, "r_colbert_admin") = True Then
+            Response.Redirect("~/admin/viewAllcars.aspx")
+        Else : Response.Redirect("~/viewAllcars.aspx")
+        End If
+    End Sub
+End Class
